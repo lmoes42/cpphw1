4eab2941be6442b6f729cd3fa2dd3b8de9809a9c
levi moes <levimoes@levis-MacBook-Pro.local>
2021-09-20 19:38:23 +0200

    Added Exercise 4

---
+++ RSL.cc
@@ -0,0 +1,28 @@
+/******************************************************************************
+* File:             RSL.cpp
+*
+* Author:           Levi Moes
+* Created:          09/20/21
+* Description:      Output some text as a raw string literal
+* Inspired by this stackoverflow post
+* https://stackoverflow.com/questions/30308088/include-in-raw-string-literal-without-terminating-said-literal
+*****************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+// Makes my syntax checker flood me with errors
+// but hey, it compiles
+// exercise said I had to write this outside my main,
+// dunno why
+string output = R"###(^\s+Encryption key:(\w+)
+^\s+Quality=(\d+)
+^\s+E?SSID:"([[:print:]]+)"
+^\s+ssid="([[:print:]]+)")###";
+// As the string we want to print contains "( we
+// have to use some different pattern which it doesn't contain
+
+int main()
+{
+    std::cout << output;    // output output
+}
