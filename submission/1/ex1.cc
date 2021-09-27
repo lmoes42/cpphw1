c53cc6a03dbbb0b036a186477d28a75021b57b3a
levi moes <levimoes@levis-MacBook-Pro.local>
2021-09-27 20:27:19 +0200

    Folder structure

---
+++ submission/1/ex1.cc
@@ -0,0 +1,10 @@
+/******************************************************************************
+* File:             hello.cc
+*                   This program prints "Hello World" followed by a newline
+*****************************************************************************/
+#include <iostream>                         // Required to print the way we have been taught
+                                            //
+int main()                                  // Declaration of the main function, code which will be run
+{                                           //
+    std::cout << "Hello World" << '\n';     // Print the text "Hello World" to the commandline followed by a newline
+}                                           //
